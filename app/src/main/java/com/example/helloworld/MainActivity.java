1b581054c3d1b2062eb9d260dd02b84283409c18
AugustoJmnz <2002010@utrivieramaya.edu.mx>
2022-02-01 15:26:25 -0500

    buttom

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,14 +1,19 @@
-package com.example.helloworld;
+package com.example.helloworld;//package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(v -> Toast.makeText(getApplicationContext(),
+                "He hecho clic en el Botón", Toast.LENGTH_LONG).show());
     }
 }
